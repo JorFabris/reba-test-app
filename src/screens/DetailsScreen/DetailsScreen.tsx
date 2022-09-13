8d7f4902e9a1420fa9db368b8d618e126fd9a756
Jorge Fabris <jorge.fabris2@gmail.com>
2022-09-13 17:11:32 -0300

    Added home screen and details screen

---
+++ src/screens/DetailsScreen/DetailsScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import styles from './DetailsScreenStyles';
+
+const DetailsScreen = () => {
+    return (
+        <View>
+            <Text>DetailsScreen</Text>
+        </View>
+    );
+};
+
+export default DetailsScreen;
---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import styles from './HomeScreenStyles';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
